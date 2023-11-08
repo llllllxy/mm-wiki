0635c3ffeb01fb80091af24da420880b2c28c7e9
17862719591 <184974699@qq.com>
2023-11-08 10:24:25 +0800

    fix: 升级为0.2.4版本

--- global/system.go
+++ global/system.go
@@ -1,6 +1,6 @@
 package global
 
 const (
-	SYSTEM_VERSION   = "v0.2.3"              // system version code
+	SYSTEM_VERSION   = "v0.2.4"              // system version code
 	SYSTEM_COPYRIGHT = "2018 - 2022 phachon" // system copyright
 )
